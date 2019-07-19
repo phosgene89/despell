4d78dde436aab04a8f201e6953615b3e99cda6a4
phosgene89 <34409398+phosgene89@users.noreply.github.com>
2019-07-19 19:58:42 +0930

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from distutils.core import setup
 setup(
-  name = 'despell',         # How you named your package folder (MyLib)
-  packages = ['despell'],   # Chose the same as "name"
-  version = '0.1.0',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'Greg Feldmann',                   # Type in your name
+  name = 'despell',        
+  packages = ['despell'],   
+  version = '0.2.0',      
+  license='MIT',        
+  description = 'TYPE YOUR DESCRIPTION HERE',
+  author = 'Greg Feldmann',                  
   author_email = 'felga001@mymail.unisa.edu.au,      # Type in your E-Mail
-  url = 'https://github.com/phosgene89/despell',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/phosgene89/despell/archive/v0.1.0.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[],
+  url = 'https://github.com/phosgene89/despell',  
+  download_url = 'https://github.com/phosgene89/despell/archive/v0.1.0.tar.gz',   
+  keywords = ['spelling', 'typo'],   # Keywords that define your package best
+  install_requires=['numpy'],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',    'License :: OSI Approved :: MIT License',   # Again, pick a license    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    'Development Status :: 3 - Alpha',      
+    'Topic :: Software Development :: Build Tools',    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3.6',
   ],
 )
