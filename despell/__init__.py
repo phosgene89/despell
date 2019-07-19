cd61c56a2144769e6d0a328182827ac68fe7fa12
phosgene89 <34409398+phosgene89@users.noreply.github.com>
2019-07-19 20:23:11 +0930

    Update __init__.py

--- despell/__init__.py
+++ despell/__init__.py
@@ -1,2 +1,5 @@
 name = "despell"
+import numpy as np
+import random
+from replace_dicts import qwerty_replace_dict, phonetic_replace_dict, caps_replace_dict, complex_replace_dict
 from despell import *
