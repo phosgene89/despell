0330e37c939fe331e39a8e92126bc6d74f68cb5a
Greg_Feldmann <gfeldman@tpg.com.au>
2019-10-18 20:47:12 +1030

    update

--- testing/unit_testing.py
+++ testing/unit_testing.py
@@ -1,36 +1,43 @@
 import os
-os.chdir("/media/greg/Storage/packages/spelling_distortion")
+os.chdir("/media/greg/Storage/packages/spelling_distortion/despell_pkg/despell/despell")
 
-from despell.despell import *
-
+import importlib
 import unittest
+
+import despell as ds
+
+#importlib.reload(despell)
 
 class TestCorruptText(unittest.TestCase):
     
     def test1_transpose(self):
-        self.assertFalse(type(transpose([]))==list)
+
+        with self.assertRaises(AssertionError):
+            ds.transpose([])
 
     def test_single_input_transpose(self):
-        self.assertFalse(type(transpose(["1"]))==list)
+
+        with self.assertRaises(AssertionError):
+            ds.transpose(["1"])
 
     def test_input_is_list_transpose(self):
 
-        self.assertTrue(type(transpose(["1","2"]))==list)
-        self.assertTrue(type(transpose(["a","d"]))==list)
+        self.assertTrue(type(ds.transpose(["1","2"]))==list)
+        self.assertTrue(type(ds.transpose(["a","d"]))==list)
 
     def test_output_elements_are_str_transpose(self):
 
         test_input = ["1","2"]
 
         for i in range(len(test_input)):
-            self.assertTrue(type(transpose(test_input)[i])==str)
+            self.assertTrue(type(ds.transpose(test_input)[i])==str)
 
     def test6_transpose(self):
 
         test_input = ["a","d"]
 
         for i in range(len(test_input)):
-            self.assertTrue(type(transpose(test_input)[i])==str)
+            self.assertTrue(type(ds.transpose(test_input)[i])==str)
 
 if __name__ == '__main__':
 
